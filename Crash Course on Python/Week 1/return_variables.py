731d72b82042cef4cc0eb9bdb9a57a0469e95d77
Gary N. Thomas <45762811+garynth41@users.noreply.github.com>
2020-03-18 22:45:16 -0400

    wk1 course load

---
+++ Crash Course on Python/Week 1/return_variables.py
@@ -0,0 +1,7 @@
+def get_seconds(hours, minutes, seconds):
+  return 3600*hours + 60*minutes + seconds
+
+amount_a = get_seconds(2,30,0)
+amount_b = get_seconds(0,45,15)
+result = amount_a + amount_b
+print(result)
